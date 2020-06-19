6fbf61b8b606209582d831d0b8ba5df871857c02
kleofas97 <34525220+kleofas97@users.noreply.github.com>
2020-06-19 23:08:40 +0200

    "year and month visualised"

--- GenealogyVisualised.py
+++ GenealogyVisualised.py
@@ -1,4 +1,15 @@
 import pandas as pd
-
+import matplotlib.pyplot as plt
 file = pd.read_csv("jany_csv.csv", sep = ';', engine='python')
 print(file.head())
+
+# Histogram of years
+
+year = file['rok']
+lastyear = year.max()
+firstyear = year.min()
+plt.hist(year, bins=int((lastyear-firstyear)/2),color='green')
+plt.show()
+month = file['miesiac']
+plt.hist(month,bins=12)
+plt.show()
